0037ac44c201e1966530ec6ffd5e8a339a95e02c
adinaifrn11 <162191818+adinaifrn11@users.noreply.github.com>
2025-09-16 20:41:48 +0000

    aula dia 16 rotas

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Teste 123!'
+    return 'ola mundo!'
+@app.route('/contato')
+
+def contato():
+    return  '<h1>aaaann@1746</h1>'
+
+@app.route('/exemplo')
+def exemplo():
+    return render_template ( 'exemplo.html')
+
+
+
 
 if __name__ == '__main__':
     app.run()
