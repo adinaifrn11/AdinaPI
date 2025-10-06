a032b1d2e97f3b26d03eea36526cc4bb43f27068
adinaifrn11 <162191818+adinaifrn11@users.noreply.github.com>
2025-10-06 17:25:38 +0000

    aula de adicionar imagens

--- app.py
+++ app.py
@@ -5,16 +5,33 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'ola mundo!'
-@app.route('/contato')
 
+@app.route('/contato')
 def contato():
-    return  '<h1>aaaann@1746</h1>'
+    return render_template('conato.html', nome = 'adina',email = 'adina@gmail.com')
+
+
 
 @app.route('/exemplo')
 def exemplo():
     return render_template ( 'exemplo.html')
 
+@app.route('/exemplo2')
+def exemplo2():
+    return render_template('exemplo2.html')
 
+@app.route('/perfil', defaults={'nome': 'adina'})
+@app.route ('/perfil/<nome>')
+def perfil (nome):
+    return render_template('perfil.html')
+
+
+
+@app.route('/semestre/<int:x>')
+def semestre(x):
+    y = x = 1
+    return render_template('semestre.html', x=x, y=y)
+    
 
 
 if __name__ == '__main__':
