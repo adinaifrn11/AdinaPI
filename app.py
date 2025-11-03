67fd294cb7a1dccddac1fc98a2c409e465fe4bf5
adinaifrn11 <162191818+adinaifrn11@users.noreply.github.com>
2025-11-03 17:23:35 +0000

    aula do dia 03/11

--- app.py
+++ app.py
@@ -25,14 +25,45 @@ def exemplo2():
 def perfil (nome):
     return render_template('perfil.html')
 
-
-
 @app.route('/semestre/<int:x>')
 def semestre(x):
     y = x = 1
     return render_template('semestre.html', x=x, y=y)
+
+
+@app.route('/login.hml')
+def login():
+    return render_template('login.html')
+
     
+@app.route('/verificarlogin', methods=['post'])
+def verificarlogin():
+    login = request.form ['login']
+    senha = request.form['senha']
+    if login == 'admim' and senha == '12345':
+        return 'seja bem vindo admim'
+    else:
+        return 'voce não tem permissão de acessar essa pagina'
+    
+@app.route('/verificaridade/<int:idade>')
+def verificaridade(idade):
+    return render_template("verificar.html", idade=idade)
+
+@app.route('/usuario/<nome>')
+def usuario(nome):
+    return render_template('usuario.html', nome=nome)	
 
 
+@app.route('/produtos/<int:qtd')
+def produtos (qtd):
+    return render_template('produtos.html', qtd=qtd)
+
+@app.rourte('/compras')
+def compras():
+    items=['arroz','feijão','farinha','macrrao','acucar']
+    return render_template("compras.html", items=items)
+
+    
+	
 if __name__ == '__main__':
     app.run()
