f94e0b6ec0186fc91668f56975498af2e6d87ee2
clarapotter <mc4136254@gmail.com>
2026-04-30 05:45:37 -0300

    finished

---
+++ apirest/app/models/spot.py
@@ -0,0 +1,9 @@
+from app.extensions import db
+
+class ParkingSpot(db.Model):
+    __tablename__ = "spots"
+
+    id = db.Column(db.Integer, primary_key=True)
+    codigo = db.Column(db.String(10), nullable=False)
+    ocupada = db.Column(db.Boolean, default=False)
+    parking_id = db.Column(db.Integer, db.ForeignKey("parkings.id"), nullable=False)
